a10e043366cf23d51fcce4e0636dacefe79a10ca
Travis Paul <Tr@visPaul.me>
2016-04-07 19:02:08 -0400

    Rewrite index.js to rely on a simple bind

--- index.js
+++ index.js
@@ -1,73 +1,28 @@
-var crypto = require('crypto')
-var assert = require('assert')
-var LdapAuth = require('ldapauth-fork')
-var parseDN = require('ldapjs').parseDN
+var ldap = require('ldapjs');
 
-module.exports = Auth
+module.exports = Auth;
 
 function Auth(config, stuff) {
-  var self = Object.create(Auth.prototype)
-  self._users = {}
-
-  // config for this module
-  self._config = config
-
-  // sinopia logger
-  self._logger = stuff.logger
-
-  // TODO: Set more defaults
-  self._config.groupNameAttribute = self._config.groupNameAttribute || 'cn'
-
-  return self
+    var self = Object.create(Auth.prototype);
+    self._users = {};
+    self._config = config;
+    self._logger = stuff.logger;
+    self._ldap_client = ldap.createClient(self._config.client_options);
+    return self;
 }
 
-//
-// Attempt to authenticate user against LDAP backend
-//
 Auth.prototype.authenticate = function(user, password, callback) {
-  var self = this
-  var LdapClient = new LdapAuth(self._config.client_options)
-
-  LdapClient.authenticate(user, password, function(err, ldap_user) {
-    if (err) {
-      // 'No such user' is reported via error
-      self._logger.warn({
-        user: user,
-        err: err,
-      }, 'LDAP error @{err}')
-
-      LdapClient.close(function(err) {
-        if (err) {
-          self._logger.warn({
-             err: err
-            }, 'LDAP error on close @{err}')
-         }
-      })
-
-      return callback(null, false)
-    }
-
-    if (ldap_user) {
-      var groups = [ user ]
-      if ('memberOf' in ldap_user) {
-        if (!Array.isArray(ldap_user.memberOf)) {
-          ldap_user.memberOf = [ ldap_user.memberOf ]
+    var self = this;
+    self._ldap_client.bind(user, password, function(error, res) {
+        if (error) {
+            if (error.name === "InvalidCredentialsError") {
+                self._logger.info({user: user}, "Invalid Credentials for user: @{user}");
+                return callback(null, false);
+            } else {
+                self._logger.error({err: error}, "LDAP bind failed: @{err}");
+                return callback("Unable to authenticate against LDAP");
+            }
         }
-        for (var i = 0; i < ldap_user.memberOf.length; i++) {
-          groups.push("%" + parseDN(ldap_user.memberOf[i]).rdns[0][self._config.groupNameAttribute])
-        }
-      }
-    }
-
-    callback(null, groups)
-
-    LdapClient.close(function(err) {
-      if (err) {
-        self._logger.warn({
-           err: err
-          }, 'LDAP error on close @{err}')
-       }
-    })
-
-  })
+        return callback(null, [user]);
+    });
 }
