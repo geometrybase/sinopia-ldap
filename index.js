851ed29548d5ff3cf4b0a3192910a246e1323ff6
Travis Paul <Tr@visPaul.me>
2016-04-08 13:47:34 -0400

    Move client creation into authenticate method so we can unbind

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ function Auth(config, stuff) {
     self._users = {};
     self._config = config;
     self._logger = stuff.logger;
-    self._ldap_client = ldap.createClient(self._config.client_options);
     return self;
 }
 
-Auth.prototype.authenticate = function(user, password, callback) {
-    var self = this, domain = self._config.domain;
+Auth.prototype.authenticate = function(user, password, auth_callback) {
+    var self = this,
+        domain = self._config.domain,
+        client = ldap.createClient(self._config.client_options),
+        callback = function(error, groups) {
+            client.unbind();
+            return auth_callback(error, groups);
+        };
     if (domain) {
-        if (user.indexOf('@') !== -1 && user.split("@").pop() !== domain) {
+        if (user.indexOf("@") !== -1 && user.split("@").pop() !== domain) {
             self._logger.warn({user: user}, "Rejected auth due to invalid domain: @{user}");
             return callback(null, false);
         }
@@ -22,7 +27,7 @@ Auth.prototype.authenticate = function(user, password, callback) {
             user += "@" + domain;
         }
     }
-    self._ldap_client.bind(user, password, function(error) {
+    client.bind(user, password, function(error) {
         var groups = [];
         if (error) {
             if (error.name === "InvalidCredentialsError") {
